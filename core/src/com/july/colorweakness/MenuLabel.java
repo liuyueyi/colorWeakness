67e79753289316824e3058fc286893adc909aa34
wzb <bangzewu@126.com>
2014-10-31 20:13:23 +0800

    alter

--- core/src/com/july/colorweakness/MenuLabel.java
+++ core/src/com/july/colorweakness/MenuLabel.java
@@ -11,33 +11,15 @@ public class MenuLabel extends Label {
 	final static int CENTER = 0;
 	final static int RIGHT = 1;
 
-	/**
-	 * 构造菜单标签
-	 * 
-	 * @param text
-	 *            菜单显示的文字
-	 * @param style
-	 *            白底黑字 和 黑底白字两种
-	 * @param type
-	 *            菜单的类型，是一级菜单还是二级菜单 [MENU, SUBMENU]
-	 */
-	public MenuLabel(CharSequence text, LabelStyle style, int type) {
+	public MenuLabel(CharSequence text, LabelStyle style) {
 		super(text, style);
-		setType(type);
 		setAlignment(Align.center);
 	}
 
-	final static int MENU = 1;
-	final static int SUBMENU = 2;
-
-	public void setType(int type) {
-		if (type == 1) {
-			// 顶层菜单
-			setSize(Constants.width, Constants.menuHeight);
-		} else {
-			// 子菜单
-			setSize(Constants.width, Constants.subMenuHeight);
-		}
+	public MenuLabel(CharSequence text, LabelStyle style, int index) {
+		this(text, style);
+		setSize(Constants.width, Constants.menuHeight);
+		setPosition(0, index * getHeight());
 	}
 
 	public void setMoveOutAction(int direction, float delayTime) {
